6eb443a9875c9f927686e9e413c4391fab6a0dda
Gustavo Perdomo <gperdomor@gmail.com>
2017-12-03 00:11:22 -0300

    Rework (#3)
    Rework using roles

---
+++ init.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# This file ensures that Ansible and any dependencies are in place.
+
+# Prerequisites: See README.md
+
+# Credits:
+# - https://github.com/superlumic/superlumic/blob/master/superlumic
+
+# ------------------------------------------------------------------------------
+
+# Vars
+# ------------------------------------------------------------------------------
+
+BGreen='\e[1;32m' # Green
+Color_Off='\e[0m' # Text Reset
+
+# Functions
+# ------------------------------------------------------------------------------
+
+function setStatusMessage {
+  printf " --> ${BGreen}$1${Color_Off}\n" 1>&2
+}
+
+# Check whether a command exists - returns 0 if it does, 1 if it does not
+function exists {
+  if command -v $1 >/dev/null 2>&1
+  then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# Install
+# ------------------------------------------------------------------------------
+
+# Install brew
+if ! exists brew; then
+  setStatusMessage "Installing brew"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  setStatusMessage "Updating Brew"
+  brew update
+fi
+
+# Install Ansible
+# TODO: We need at least Ansible 2.4.0, upgrade if needed.
+if ! exists ansible; then
+  setStatusMessage "Installing Ansible"
+  brew install ansible
+else
+  setStatusMessage "Updating Ansible"
+  brew outdated ansible || brew upgrade ansible
+fi
+
+# Install pexpect
+setStatusMessage "Installing pexpect"
+pip2 install pexpect --quiet
+
+# Confirm installed Ansible version
+ansible --version
+
+# Install Ansible Galaxy dependencies:
+setStatusMessage "Installing required Ansible Galaxy roles"
+ansible-galaxy install -r requirements.yml --force
---
+++ roles/README.md
@@ -0,0 +1,11 @@
+# Roles
+
+Add your custom roles here if you want to keep them within the same git repository.
+
+Otherwise, consider downloading roles from elsewhere using Ansible Galaxy (see requirements.yml).
+
+## Examples
+
+Here's an example of a working, forked repository that uses its own custom roles as well as roles from Ansible Galaxy:
+
+https://github.com/danbohea/macsible
